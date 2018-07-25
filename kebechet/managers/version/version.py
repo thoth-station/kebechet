4d388e82bad64ab8ddbfb1c6cfe2db34cfe57cfc
Fridolin Pokorny <fridolin@redhat.com>
2018-07-25 13:24:48 +0200

    Implement version change logic

--- kebechet/managers/version/version.py
+++ kebechet/managers/version/version.py
@@ -17,10 +17,14 @@
 
 """Automatically issue a new PR with adjusted version for Python projects."""
 
+import os
 import logging
 
 from kebechet.utils import cloned_repo
 from kebechet.managers.manager import ManagerBase
+
+from git import Repo
+from IGitt.Interfaces.Issue import Issue
 
 _LOGGER = logging.getLogger(__name__)
 _VERSION_REQUEST_ISSUE = ' release'
@@ -30,9 +34,70 @@ _VERSION_PULL_REQUEST_NAME = 'Release of version {}'
 class VersionManager(ManagerBase):
     """Automatic version management for Python projects."""
 
-    def _adjust_version_in_sources(self, version_identifier: str):
-        # TODO: Implement
-        return
+    @staticmethod
+    def _adjust_version_file(file_path: str, new_version: str):
+        """Adjust version in the given file, return signalizes whether the return value indicates change in file."""
+        with open(file_path, 'r') as input_file:
+            content = input_file.read().splitlines()
+
+        changed = False
+        for idx, line in enumerate(content):
+            if line.startswith('__version__ = '):
+                parts = line.split(' = ', maxsplit=1)
+                if len(parts) != 2:
+                    _LOGGER.warning(
+                        "Found '__version__' identifier but unable to parse old version, skipping: %r", line
+                    )
+                    continue
+
+                old_version = parts[1]
+                _LOGGER.info("Old version found in sources: %r", old_version)
+
+                content[idx] = f'__version__ = "{new_version}"'
+                changed = True
+
+        if not changed:
+            return False
+
+        # Apply changes.
+        with open(file_path, 'w') as output_file:
+            output_file.write("\n".join(content))
+            # Add new line at the of file explicitly.
+            output_file.write("\n")
+
+        return True
+
+    def _adjust_version_in_sources(self, repo: Repo, new_version: str, labels: list, issue: Issue):
+        """Walk through the directory structure and try to adjust version identifier in sources."""
+        adjusted_count = 0
+        for root, _, files in os.walk('./'):
+            for file_name in files:
+                if file_name in ('setup.py', '__init__.py'):
+                    file_path = os.path.join(root, file_name)
+                    adjusted = self._adjust_version_file(file_path, new_version)
+                    if adjusted:
+                        repo.git.add(file_path[len(os.getcwd()):])
+                        adjusted_count += 1
+
+        if adjusted_count == 0:
+            error_msg = f"No version identifier found in sources to release {new_version}"
+            _LOGGER.warning(error_msg)
+            self.sm.open_issue_if_not_exist(
+                error_msg,
+                lambda x: "Automated version release cannot be performed.\nRelated: #" + str(issue.number),
+                labels
+            )
+
+        if adjusted_count > 1:
+            error_msg = f"Multiple version identifiers found in sources to release {new_version}"
+            _LOGGER.warning(error_msg)
+            self.sm.open_issue_if_not_exist(
+                error_msg,
+                lambda x: "Automated version release cannot be performed.\nRelated: #" + str(issue.number),
+                labels
+            )
+
+        return adjusted_count == 1
 
     def run(self, labels: list = None) -> None:
         """Check issues for new issue request, if a request exists, issue a new PR with adjusted version in sources."""
@@ -52,7 +117,9 @@ class VersionManager(ManagerBase):
             branch_name = 'v' + version_identifier
 
             with cloned_repo(self.service_url, self.slug) as repo:
-                self._adjust_version_in_sources(version_identifier)
+                if not self._adjust_version_in_sources(version_identifier, labels, issue):
+                    _LOGGER.error("Giving up with automated release")
+                    return
 
                 repo.git.checkout('HEAD', b=branch_name)
                 repo.git.tag(version_identifier)
