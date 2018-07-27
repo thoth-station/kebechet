1816d89b7578390a0c6b68d17f335781daee59d5
Fridolin Pokorny <fridolin@redhat.com>
2018-07-27 14:50:40 +0200

    Use dev release of IGitt

--- setup.py
+++ setup.py
@@ -32,8 +32,5 @@ setup(
     author_email='fridolin@redhat.com',
     license='GPLv3+',
     packages=find_packages(),
-    install_requires=get_install_requires(),
-    dependency_links=[
-        'git+https://gitlab.com/gitmate/open-source/IGitt.git#egg=IGitt',
-    ]
+    install_requires=get_install_requires()
 )
