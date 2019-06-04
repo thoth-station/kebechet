9780c711f99f83d00f3fcddd32786de58cf05346
Kevin Postlethwait <k.postlethwait24@gmail.com>
2019-06-04 16:50:35 -0400

    Forgot to included init that makes manager accesible

--- kebechet/managers/__init__.py
+++ kebechet/managers/__init__.py
@@ -4,10 +4,12 @@ from .info import InfoManager
 from .update import UpdateManager
 from .version import VersionManager
 from .pipfile_requirements import PipfileRequirementsManager
+from .thoth import ThothManager
 
 REGISTERED_MANAGERS = {
     'update': UpdateManager,
     'info': InfoManager,
     'version': VersionManager,
-    'pipfile-requirements': PipfileRequirementsManager
+    'pipfile-requirements': PipfileRequirementsManager,
+    'thoth': ThothManager
 }
