9c788b1356c285de5175c0bb6e05c5571f533004
Kevin Postlethwait <k.postlethwait24@gmail.com>
2019-07-03 14:32:54 -0400

    Back to docker

--- app.sh
+++ app.sh
@@ -4,12 +4,31 @@
 # sub-command based on env-variables configuration.
 #
 
+# Check whether there is a passwd entry for the container UID
+myuid=$(id -u)
+mygid=$(id -g)
+uidentry=$(getent passwd $myuid)
+
+# If there is no passwd entry for the container UID, attempt to create one
+if [ -z "$uidentry" ] ; then
+    if [ -w /etc/passwd ] ; then
+        echo "$myuid:x:$myuid:$mygid:anonymous uid:/home/user:/bin/false" >> /etc/passwd
+    else
+        echo "Container ENTRYPOINT failed to add passwd entry for anonymous UID"
+    fi
+fi
+# The git_ssh_command helps the server by pass the Host key checking while connecting to github.
+export GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
 case $KEBECHET_SUBCOMMAND in
     'run-url') 
-        exec /opt/app-root/bin/python3 kebechet-cli run-url $REPO_URL $SERVICE_NAME
+        exec pipenv run python3 kebechet-cli run-url $REPO_URL $SERVICE_NAME
         ;;
     'run-results') 
-        exec /opt/app-root/bin/python3 kebechet-cli run-results $REPO_URL $SERVICE_NAME $ANALYSIS_ID
+        exec pipenv run python3 kebechet-cli run-results $REPO_URL $SERVICE_NAME $ANALYSIS_ID
+        ;;
+    'run')
+        exec pipenv run python3 kebechet-cli run # TODO
         ;;
     *)
         echo "Application configuration error - invalid or no subcommand supplied"
