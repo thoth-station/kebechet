af9a240aef02dfcce99127baf3fbce17650b4796
Marek Cermak <macermak@redhat.com>
2019-10-02 16:59:05 +0200

    Relock dependencies and update requirements
    - make sure `get_install_requires` correctly parses requirements
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   Pipfile.lock
    modified:   requirements.txt
    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 from setuptools import setup, find_packages
 from pathlib import Path
 
@@ -7,7 +8,23 @@ def get_install_requires():
     with open('requirements.txt', 'r') as requirements_file:
         # TODO: respect hashes in requirements.txt file
         res = requirements_file.readlines()
-        return [req.split(' ', maxsplit=1)[0] for req in res if req]
+
+        requirements = []
+        for req in res:
+            req = req.strip('\n')
+            if not req:
+                continue
+
+            if req.startswith('-e'):  # replace with PEP-508 specification
+                url = req.strip('-e ')
+                pkg = url.split('#egg=', maxsplit=1)[-1]
+                dep = pkg + " @ " + url
+            else:
+                dep = req.split(' ', maxsplit=1)[0]
+
+            requirements.append(dep)
+
+    return requirements
 
 
 def get_version():
