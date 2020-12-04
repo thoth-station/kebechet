fd34bee1bc7817544f4de5627a2a54172a2ba985
Kebechet <noreply+kebechet@redhat.com>
2020-12-04 14:28:28 +0000

    Release of version 1.2.2

--- kebechet/__init__.py
+++ kebechet/__init__.py
@@ -5,5 +5,5 @@ from .managers import InfoManager  # noqa F401
 from .managers import UpdateManager  # noqa F401
 
 __name__ = "kebechet"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __author__ = "Fridolin Pokorny <fridolin.pokorny@gmail.com>"
