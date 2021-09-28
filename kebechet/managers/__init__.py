559d170a975bcaafe1522f0c2a3076784e6457a5
Kevin Postlethwait <k.postlethwait24@gmail.com>
2021-09-28 16:51:18 -0400

    add custom exception for known failure cases and open issues for failed manager

--- kebechet/managers/__init__.py
+++ kebechet/managers/__init__.py
@@ -9,6 +9,7 @@ from .thoth_provenance import ThothProvenanceManager
 from .label_bot import ThothLabelBotManager
 from .config_initializer import ConfigInitializer  # noqa F401
 from .manager import ManagerBase  # noqa F401
+from .manager import ManagerFailedException  # noqa F401
 
 REGISTERED_MANAGERS = {
     "update": UpdateManager,
