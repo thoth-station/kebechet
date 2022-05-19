a5aeffec95cab5317d0d77c3ad2f5ab0c850f3c4
Kebechet <noreply+kebechet@redhat.com>
2022-05-19 17:21:54 +0000

    Release of version 1.9.0

--- kebechet/__init__.py
+++ kebechet/__init__.py
@@ -1,5 +1,5 @@
 """Update dependencies stated in the Pipfile.lock for the given repo or repositories."""
 
 __name__ = "kebechet"
-__version__ = "1.8.0"
+__version__ = "1.9.0"
 __author__ = "Fridolin Pokorny <fridolin.pokorny@gmail.com>"
