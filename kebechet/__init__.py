7ea414a00cc2b1d1df91e20a8df2ae72812377a3
Kebechet <noreply+kebechet@redhat.com>
2022-11-14 11:30:44 +0000

    Release of version 1.11.0

--- kebechet/__init__.py
+++ kebechet/__init__.py
@@ -1,5 +1,5 @@
 """Update dependencies stated in the Pipfile.lock for the given repo or repositories."""
 
 __name__ = "kebechet"
-__version__ = "1.10.5"
+__version__ = "1.11.0"
 __author__ = "Fridolin Pokorny <fridolin.pokorny@gmail.com>"
